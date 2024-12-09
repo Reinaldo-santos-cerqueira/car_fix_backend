a637b58d348b1dc6b6f4b15da49d5c8e4b6719b3
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-12-09 10:35:02 -0300

    chore: starting project

---
+++ src/main/java/dev/reinaldosantos/car_fix/CarFixApplication.java
@@ -0,0 +1,13 @@
+package dev.reinaldosantos.car_fix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/reinaldosantos/car_fix/CarFixApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.reinaldosantos.car_fix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
