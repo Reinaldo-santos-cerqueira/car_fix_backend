8aa825776be79b3c24fe562212fd6a94e57597ce
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2024-12-11 09:35:09 -0300

    feat: retrieving the token and filtering it and adding the filter to the security config and adding routes with permission to everyone

--- src/main/java/dev/reinaldosantos/car_fix/config/SecurityConfig.java
+++ src/main/java/dev/reinaldosantos/car_fix/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package dev.reinaldosantos.car_fix.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,14 +12,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(crsf -> crsf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorize -> authorize
+            .requestMatchers(HttpMethod.POST,"/authentication/signup/serivce_provider").permitAll()
+            .requestMatchers(HttpMethod.POST,"/authentication/signup/client").permitAll()
+            .requestMatchers(HttpMethod.POST,"/authentication/login").permitAll()
+            .requestMatchers(HttpMethod.PATCH,"/changePassword").permitAll()
+        )
+        .addFilterBefore(securityFilter,UsernamePasswordAuthenticationFilter.class)
         .build();
     }
 
