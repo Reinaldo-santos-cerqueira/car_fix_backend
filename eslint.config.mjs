dd4703fbb043d1f11c97fb974a34fa52c6e78662
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-04 00:09:45 -0300

    chore: updating eslint rules, tsconfig rules, new libs and remove null permission in some property in schema primas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,24 +3,24 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
-    rules: {
-      "no-console": "warn",
-      "eqeqeq": "error",
-      "curly": "error",
-      "quotes": ["error", "double"], 
-      "semi": ["error", "always"],
-      "indent": ["error", 2],
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        rules: {
+            "no-console": "warn",
+            "eqeqeq": "error",
+            "curly": "error",
+            "quotes": ["error", "double"], 
+            "semi": ["error", "always"],
+            "indent": ["error", 4],
+        },
     },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["warn"],
-      "@typescript-eslint/explicit-function-return-type": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": ["warn"],
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/no-explicit-any": "warn",
+        },
     },
-  },
 ];
--- prisma/migrations/20250303215039_create_table_address/migration.sql
+++ prisma/migrations/20250303215039_create_table_address/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE "Address" (
     "id" UUID NOT NULL,
-    "neighborhood" TEXT,
-    "street" TEXT,
-    "number" TEXT,
-    "city" TEXT,
-    "state" TEXT,
-    "cep" TEXT,
+    "neighborhood" TEXT NOT NULL,
+    "street" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
     "complement" TEXT,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
