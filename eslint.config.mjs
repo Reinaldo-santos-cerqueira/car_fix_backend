d163eb891e1805b2227540d90f3715f5efc19e8f
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-03 22:24:03 -0300

    chore: init project with configuration to nodemon typescript eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "no-console": "warn",
+      "eqeqeq": "error",
+      "curly": "error",
+      "quotes": ["error", "double"], 
+      "semi": ["error", "always"],
+      "indent": ["error", 2],
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+];
