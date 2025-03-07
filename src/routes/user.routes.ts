0890a6fb78c7f2b6ca117ebcd9c5bbf76cb41bc9
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-07 18:09:56 -0300

    feat: implements routes to create service provider

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,22 @@ import express, { Request, Response } from "express";
 import { ValidationDtoMiddlewares,uploadImage } from "@middlewares";
 import { UserController } from "@controllers";
 
-
 const userRouter = express.Router();
 const controller = new UserController();
 
-userRouter.post("/users",uploadImage.single("imageDocumentVehicle"),ValidationDtoMiddlewares.validateUser(), async (req: Request, res: Response) => {    
-    return await controller.save(req,res);
+userRouter.post("/users/client",uploadImage.single("imageDocumentVehicle"),ValidationDtoMiddlewares.validateUser(), async (req: Request, res: Response) => {
+    return await controller.saveClient(req,res);
 });
+
+userRouter.post("/users/service_provider",
+    uploadImage.fields([
+        {name:"imageDocumentVehicle",maxCount:1},
+        { name: "imageCnh", maxCount: 1 }
+    ]),
+    ValidationDtoMiddlewares.validateUser(), 
+    async (req: Request, res: Response) => {
+        return await controller.saveClient(req,res);
+    });
+    
 
 export { userRouter };
