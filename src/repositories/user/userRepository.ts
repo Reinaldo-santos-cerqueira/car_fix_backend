58102c8930b1c1ee1e62c59b47d348d429b46434
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-07 17:40:36 -0300

    feat: addinting function create service provider and verify cnh exists

---
+++ src/repositories/user/userRepository.ts
@@ -0,0 +1,47 @@
+import { PrismaClient, Prisma, User } from "@prisma/client";
+
+export class UserRepository{
+    prisma = new PrismaClient();
+
+    async saveClient(user: Prisma.UserCreateInput): Promise<Prisma.UserCreateInput> {
+        return await this.prisma.user.create({
+            data: user
+        });
+    }
+
+    async saveServiceProvider(user: Prisma.UserCreateInput): Promise<{ ServiceProvider: { id: string; created_at: Date; updated_at: Date; path_to_image_cnh: string | null; cnh: string; user_id: string; }[]}> {
+        return await this.prisma.user.create({
+            data: user,
+            select:{
+                ServiceProvider: true
+            }
+        });
+    }
+
+
+    async findUserByIdentifierOrEmail(identifier: string, email: string): Promise<Prisma.UserCreateInput | null> {      
+        return await this.prisma.user.findFirst({
+            where: {
+                OR: [
+                    { identifier: identifier },
+                    { email: email },
+                ],
+            },
+        });
+    }
+
+    async findUserByIdentifierOrEmailorCnh(identifier: string, email: string,cnh: string): Promise<User | null> {      
+        return await this.prisma.user.findFirst({
+            where: {
+                OR: [
+                    { identifier: identifier },
+                    { email: email },
+                    {ServiceProvider: {some: {cnh: cnh}},}
+                ],
+            },
+            include:{
+                ServiceProvider: true
+            }
+        });
+    }
+}
