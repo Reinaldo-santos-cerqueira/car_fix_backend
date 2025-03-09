ce308834c525c030c92f9b8f9f63cedd93fc5d22
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-09 13:34:17 -0300

    feat: implements middlewares for login validation dto

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,4 @@ export * from "./errorHandler.middlewares";
 export * from "./validationUserDto.middlewares";
 export * from "./uploadImage.middlewares";
 export * from "./validationServiceProviderDto.middlewares";
+export * from "./validationLoginDto.middlewares";
