c10ddbdd8513c1f80af2fcbe94e8818750580cb1
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-09 13:16:58 -0300

    refactor:  change name of class

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,23 +1,24 @@
 import express, { Request, Response } from "express";
-import { ValidationDtoMiddlewares,uploadImage } from "@middlewares";
+import {ValidationServiceProviderDtoMiddlewares, ValidationUserDtoMiddlewares, uploadImage } from "@middlewares";
 import { UserController } from "@controllers";
 
 const userRouter = express.Router();
 const controller = new UserController();
 
-userRouter.post("/users/client",uploadImage.single("imageDocumentVehicle"),ValidationDtoMiddlewares.validateUser(), async (req: Request, res: Response) => {
-    return await controller.saveClient(req,res);
+userRouter.post("/users/client", uploadImage.single("imageDocumentVehicle"), ValidationUserDtoMiddlewares.validateUser(), async (req: Request, res: Response) => {
+    return await controller.saveClient(req, res);
 });
 
 userRouter.post("/users/service_provider",
     uploadImage.fields([
-        {name:"imageDocumentVehicle",maxCount:1},
+        { name: "imageDocumentVehicle", maxCount: 1 },
         { name: "imageCnh", maxCount: 1 }
     ]),
-    ValidationDtoMiddlewares.validateUser(), 
+    ValidationServiceProviderDtoMiddlewares.validateUser(),
     async (req: Request, res: Response) => {
-        return await controller.saveClient(req,res);
-    });
-    
+        return await controller.saveClient(req, res);
+    }
+);
+
 
 export { userRouter };
