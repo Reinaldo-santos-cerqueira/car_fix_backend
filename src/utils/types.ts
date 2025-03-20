33d56ef7b9b4dddfef57932edaa555a0cd70867e
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-20 18:44:03 -0300

    feat: extend types with AcceptedService and LegData interfaces

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,22 @@
+import { ServiceRequested } from "@prisma/client";
+
 export type DateString = string | Date | undefined;
 export type ServiceProviderSignupSocketIoRequest = {socketIoId: string, serviceProviderId: string,state: number }
+export type aceptService = {
+    serviceProviderId: string, 
+    serviceRequestedId: string,
+    latitudeServiceProvider: number,
+    longitudeServiceProvider: number, 
+    serviceProviderSocketId: string,
+    status: number,
+}
+
+export interface LegData {
+    steps: [];
+    summary: string;
+    weight: number;
+    duration: number;
+    distance: number;
+}
+
+export type AcceptedServiceToServiceProvider = {distance: number, duration: number, requestedService: ServiceRequested};
