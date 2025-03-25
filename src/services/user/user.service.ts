f54c9e22ce45c59396abcfd289e08797acb0425e
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2025-03-25 12:03:44 -0300

    feat: adding function to login, change password and create client/service provider

--- src/services/user/user.service.ts
+++ src/services/user/user.service.ts
@@ -1,26 +1,34 @@
-import { UserDto } from "@dto";
+import { ChangePasswordDto, LoginDto, SendTokenDto, ServiceProviderDto, UserDto } from "@dto";
 import { CustomException } from "@exceptions";
 import { Prisma } from "@prisma/client";
-import { UserRepositories } from "@repositories";
+import { UserRepository,ServiceProviderServiceRepository } from "@repositories";
 import bcrypt from "bcrypt";
 import fs from "fs";
 import path from "path";
+import { createTransport } from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export class UserService {
-    repository: UserRepositories;
+    repository: UserRepository;
+    serviceProviderServiceRepository: ServiceProviderServiceRepository;
     constructor(){
-        this.repository = new UserRepositories();
+        this.repository = new UserRepository();
+        this.serviceProviderServiceRepository = new ServiceProviderServiceRepository();
     }
-    async save(userDto: UserDto, file: Express.Multer.File | undefined): Promise<Prisma.UserCreateInput> { 
+
+    async saveClient(userDto: UserDto, file: Express.Multer.File | undefined): Promise<void> { 
         if(!file){
             throw new CustomException("Image is required", 400);
         }
         try {
             await this.findUserByIdentifierOrEmail(userDto.identifier, userDto.email);
             userDto.vehicle.path_to_document = file.path;
-            const user: Prisma.UserCreateInput = await this.createUserCreateInput(userDto);    
+            const user: Prisma.UserCreateInput = await this.createClientUserCreateInput(userDto);    
                 
-            return await this.repository.save(user);   
+            await this.repository.saveClient(user);   
+            
         } catch (error) {
             if(error instanceof CustomException ){
                 this.deleteImage(file);
@@ -28,29 +36,20 @@ export class UserService {
             }
             throw new CustomException(error + "", 500);
         }
-
     }
 
-    private async encryptPassword(password: string):Promise<string> {
-        return await bcrypt.hash(password, 10);
-    }
-
-    private async createUserCreateInput(userDto: UserDto): Promise<Prisma.UserCreateInput> {
+    private async createClientUserCreateInput(userDto: UserDto): Promise<Prisma.UserCreateInput> {
         return {
             full_name: userDto.full_name,
             phone_number: userDto.phone_number,
             email: userDto.email,
             identifier: userDto.identifier,
             password: await this.encryptPassword(userDto.password),
-            created_at: userDto.created_at,
-            updated_at: userDto.updated_at,
             token_phone: userDto.token_phone,
             token_password_change: userDto.token_password_change,
             type: userDto.type,
             address: {
                 create: {
-                    created_at: userDto.address.created_at,
-                    updated_at: userDto.address.updated_at,
                     neighborhood: userDto.address.neighborhood,
                     street: userDto.address.street,
                     number: userDto.address.number,
@@ -62,8 +61,6 @@ export class UserService {
             },
             Vehicle: {
                 create: {
-                    created_at: userDto.vehicle.created_at,
-                    updated_at: userDto.vehicle.updated_at, 
                     model: userDto.vehicle.model,
                     color: userDto.vehicle.color,
                     mark: userDto.vehicle.mark,
@@ -82,11 +79,188 @@ export class UserService {
         }
     }
 
+    async saveServiceProvider(serviceProviderDto: ServiceProviderDto, files: Express.Multer.File[] | {
+        [fieldname: string]: Express.Multer.File[];
+    } | undefined): Promise<void> { 
+        let fileImageDocumentVehicle: Express.Multer.File | undefined = undefined;
+        let fileImageCnh: Express.Multer.File | undefined = undefined;
+        if(!files){
+            throw new CustomException("Images is required", 400);
+        }
+        const fileArray: Express.Multer.File[] = Array.isArray(files) 
+            ? files 
+            : Object.values(files).flat();
+
+        for (const file of fileArray) {
+            if (file.fieldname === "imageDocumentVehicle") {
+                fileImageDocumentVehicle = file;
+            }else if(file.fieldname === "imageCnh"){
+                fileImageCnh = file;
+            }
+        }
+
+        if(!fileImageDocumentVehicle){
+            throw new CustomException("Image document vehicle is required", 400);
+        }
+        if(!fileImageCnh){
+            throw new CustomException("Image cnh is required", 400);
+        }
+        try {
+            await this.findUserByIdentifierOrEmailOrCnh(serviceProviderDto.user_dto.identifier, serviceProviderDto.user_dto.email, serviceProviderDto.cnh);
+            serviceProviderDto.user_dto.vehicle.path_to_document = fileImageDocumentVehicle.path;
+            serviceProviderDto.path_to_image_cnh = fileImageCnh.path;
+            const serviceProvider: Prisma.UserCreateInput = await this.createServiceUserCreateInput(serviceProviderDto);    
+            
+            const saveServiceProvider = await this.repository.saveServiceProvider(serviceProvider);   
+            await this.serviceProviderServiceRepository.save(saveServiceProvider.ServiceProvider[0].id, serviceProviderDto.services_id);
+            
+        } catch (error) {
+            if(error instanceof CustomException ){
+                this.deleteImage(fileImageCnh);
+                this.deleteImage(fileImageDocumentVehicle);
+                throw new CustomException(error.message, error.statusCode);
+            }
+            throw new CustomException(error + "", 500);
+        }
+    }
+
+    private async createServiceUserCreateInput(serviceProviderDto: ServiceProviderDto): Promise<Prisma.UserCreateInput> {
+        return {
+            full_name: serviceProviderDto.user_dto.full_name,
+            phone_number: serviceProviderDto.user_dto.phone_number,
+            email: serviceProviderDto.user_dto.email,
+            identifier: serviceProviderDto.user_dto.identifier,
+            password: await this.encryptPassword(serviceProviderDto.user_dto.password),
+            created_at: new Date(),
+            updated_at: new Date(),
+            token_phone: serviceProviderDto.user_dto.token_phone,
+            token_password_change: serviceProviderDto.user_dto.token_password_change,
+            type: serviceProviderDto.user_dto.type,
+            address: {
+                create: {
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                    neighborhood: serviceProviderDto.user_dto.address.neighborhood,
+                    street: serviceProviderDto.user_dto.address.street,
+                    number: serviceProviderDto.user_dto.address.number,
+                    city: serviceProviderDto.user_dto.address.city,
+                    state: serviceProviderDto.user_dto.address.state,
+                    cep: serviceProviderDto.user_dto.address.cep,
+                    complement: serviceProviderDto.user_dto.address.complement,
+                }
+            },
+            Vehicle: {
+                create: {
+                    created_at: new Date(),
+                    updated_at: new Date(), 
+                    model: serviceProviderDto.user_dto.vehicle.model,
+                    color: serviceProviderDto.user_dto.vehicle.color,
+                    mark: serviceProviderDto.user_dto.vehicle.mark,
+                    plate: serviceProviderDto.user_dto.vehicle.plate,
+                    path_to_document: serviceProviderDto.user_dto.vehicle.path_to_document
+                }
+            },
+            ServiceProvider: {
+                create: {
+                    cnh: serviceProviderDto.cnh,
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                    path_to_image_cnh: serviceProviderDto.path_to_image_cnh,
+                }
+            },
+            role: serviceProviderDto.user_dto.role,
+        };
+    }
+
+    private async findUserByIdentifierOrEmailOrCnh(identifier: string, email: string, cnh: string){
+        const userReturn =  await this.repository.findUserByIdentifierOrEmailorCnh(identifier, email,cnh);
+        if(userReturn !== null){
+            throw new CustomException("User already exists", 409);
+        }
+    }
+
+    async loginClient(loginDto: LoginDto) {
+        const user = await this.repository.findByEmail(loginDto.email);
+        if (!user || !(await this.comparePassword(loginDto.password, user.password))) {
+            throw new CustomException("Email or password incorrect", 401);
+        }
+    }
+
+    async loginServiceProvider(loginDto: LoginDto): Promise<string[]> {
+        const user = await this.repository.findByEmailAndSelectService(loginDto.email);
+        if (!user || !(await this.comparePassword(loginDto.password, user.password))) {
+            throw new CustomException("Email or password incorrect", 401);
+        }
+        const serviceIds = user?.ServiceProvider.flatMap(sp =>
+            sp.ServiceProviderService.map(sps => sps.serviceId)
+        ) || [];
+        return serviceIds;
+    }
+
+    async sendByTokenTradePassword(sendTokenDto: SendTokenDto) {
+        const user = await this.repository.findByEmail(sendTokenDto.email);
+        if (!user) {
+            throw new CustomException("Email is incorrect", 401);
+        }
+        const randomToken = this.generateCodeRandomTokenPhone();
+        this.sendEmail(user.email, "Redefinição de Senha", this.createMessageEmail("car fix enterprise", user.full_name, randomToken));
+    }
+
+    async changePassword(changePasswordDto:ChangePasswordDto) {
+        await this.repository.updatePasswordFindByEmailAndToken(changePasswordDto.email,changePasswordDto.token, changePasswordDto.password);
+    }
+
     private async deleteImage(file: Express.Multer.File){
         const filePath = path.resolve(file.path);
         fs.unlink(filePath, (err) => {
             if (err) {throw new CustomException(err.name + " " + err.code,);}
         });
     }
-          
+
+    private async encryptPassword(password: string):Promise<string> {
+        return await bcrypt.hash(password, 10);
+    }
+
+    private async comparePassword(passwordRequest: string, passwordDb: string): Promise<boolean> {
+        return await bcrypt.compare(passwordRequest, passwordDb);
+    }
+
+    private generateCodeRandomTokenPhone() {
+        const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        let code = "";
+        for (let i = 0; i < 6; i++) {
+            const randomCharacters = Math.floor(Math.random() * characters.length);
+            code += characters[randomCharacters];
+        }
+        return code;
+    }
+
+    private async sendEmail(recipient: string, subject:string, message: string) {
+        const transporter = createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASSWORD
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: recipient,
+            subject: subject,
+            text: message
+        };
+
+        await transporter.sendMail(mailOptions);
+    }
+    private createMessageEmail(enterprise: string,userName: string, randomToken: string): string {
+        return `
+            <p>Olá, ${userName},</p>
+            <p>Recebemos uma solicitação para redefinir sua senha. Para continuar com o processo, utilize o código abaixo:</p>
+            <h2 style="color: #2d89ef;">🔑 ${randomToken}</h2>
+            <p>Se você não solicitou a alteração, ignore este e-mail. O código expirará em 30 minutos.</p>
+            <p>Atenciosamente,</p>
+            <p><strong>${enterprise}</strong></p>
+        `;
+    }
 }
