34f1d56ca039bb14ceaae25775a0700b30747583
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-22 22:53:12 -0300

    fix: corrigindo bug causado pela falta / na rota

--- src/routes/service.routes.ts
+++ src/routes/service.routes.ts
@@ -3,7 +3,7 @@ import express from "express";
 const serviceRouter = express.Router();
 const controller = new ServiceController();
 
-serviceRouter.get("service", async (req, res) => {
+serviceRouter.get("/service", async (req, res) => {
     return await controller.get(req, res);
 });
 
