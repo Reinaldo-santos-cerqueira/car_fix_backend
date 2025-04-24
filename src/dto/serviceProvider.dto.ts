0df8ac15cf068c9cb67b02dd831626d16b775e91
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-04-24 16:35:23 -0300

    feat: adicionando optional ao elemento

--- src/dto/serviceProvider.dto.ts
+++ src/dto/serviceProvider.dto.ts
@@ -6,6 +6,7 @@ import {
     ValidateNested,
     IsArray,
     ArrayNotEmpty,
+    IsOptional,
 } from "class-validator";
 import { Type } from "class-transformer";
 import { UserDto } from "./user.dto";
@@ -17,18 +18,17 @@ export class ServiceProviderDto {
     @IsArray({ message: "Services is required" })
     @ArrayNotEmpty({ message: "Services cannot be empty" })
     services_id: string[];
-
     @IsString({ message: "Services is required" })
     cnh: string;
+    @IsOptional()
     @IsString({ message: "Image cnh is required" })
-    path_to_image_cnh: string;
+    path_to_image_cnh?: string;
 
     constructor(
         user_dto: UserDto,
         services_id: string[],
         cnh: string,
-        path_to_image_cnh: string,
-
+        path_to_image_cnh?: string,
     ) {
         this.user_dto = user_dto;
         this.services_id = services_id;
