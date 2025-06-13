1a4b1685553456967900d4d08ea52a84c796c2ba
Reinaldo Santos <kingnaldoreinaldo@gmail.com>
2025-06-13 17:48:18 -0300

    feat: adicionando cancel_service e change_socket_id

--- src/socketio/config.ts
+++ src/socketio/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Server as HttpServer } from "http";
 import { Server as SocketIOServer, Socket } from "socket.io";
 import { SocketController } from "@controllers";
@@ -13,19 +14,26 @@ export function setupSocket(server: HttpServer): SocketIOServer {
 
     io.on("connection", (socket: Socket) => {
         socket.on("signup_provider_service", (msg) =>
-            socketController.handleSignupProviderService(socket, msg)
+            socketController.handleSignupProviderService(socket, msg as any)
         );
 
         socket.on("request_service", (msg: string) =>
-            socketController.handleRequestService(socket, JSON.parse(msg))
+            socketController.handleRequestService(socket, msg as any)
         );
 
         socket.on("accept_service_service_provider", async (msg: string) =>
-            await socketController.handleAcceptServiceToServiceProvider(socket, JSON.parse(msg))
+            await socketController.handleAcceptServiceToServiceProvider(socket, msg as any)
         );
 
         socket.on("accept_service_client", async (msg: string) =>
-            await socketController.handleAcceptServiceToClient(socket, JSON.parse(msg))
+            await socketController.handleAcceptServiceToClient(socket, msg as any)
+        );
+        socket.on("cancel_service", async (msg: string) =>
+            await socketController.handleCanceledService(socket, msg as any)
+        );
+
+        socket.on("change_socket_id", async (msg: string) =>
+            await socketController.handlechangeSocketId(socket, msg as any)
         );
 
         socket.on("disconnect", () =>
